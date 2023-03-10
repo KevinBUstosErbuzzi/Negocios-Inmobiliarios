ca014935f8852148f24d1d73cd9b333a69e7ec38
Kevin Bustos Erbuzzi <Kevinbustoserbuzzi@hotmail.com>
2023-03-10 10:49:48 -0300

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,4 @@ miImage.onclick = function () {let miSrc = miImage.getAttribute("src");
 if (miSrc === "imagenes/5.jpg") {miImage.setAttribute("src","imagenes/2.jpg");}
 else {miImage.setAttribute("src","imagenes/5.jpg");}}
 
-let miBoton = document.querySelector('button');
-let miTitulo = document.querySelector( 'h1');
-function estableceNombreUsuario() 
-{let miNombre = prompt('Introduzca su nombre.');
-    if(!miNombre) 
-    {estableceNombreUsuario();} 
-    else {localStorage.setItem('nombre', miNombre);
-      miTitulo.innerHTML = 'Bienvenido/a ' + miNombre;}}
-  
-if (!localStorage.getItem('nombre')) {estableceNombreUsuario();}
-else {let nombreAlmacenado = localStorage.getItem('nombre');
-    miTitulo.textContent = 'Bienvenido/a ' + nombreAlmacenado;}
-miBoton.onclick = function() {estableceNombreUsuario();}
+
