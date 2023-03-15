7d9398b1237bc2b02f34bf5dc05c646bd94644da
Kevin Bustos Erbuzzi <Kevinbustoserbuzzi@hotmail.com>
2023-03-15 16:55:54 -0300

    upate

--- scripts/main.js
+++
@@ -1,4 +0,0 @@
-let miImage = document.querySelector(".Foto img");
-miImage.onclick = function () {let miSrc = miImage.getAttribute("src");
-if (miSrc === "imagenes/5.jpg") {miImage.setAttribute("src","imagenes/2.jpg");}
-else {miImage.setAttribute("src","imagenes/5.jpg");}}
