510bee747e2e6ac72b72c6178af765aa7e8bf320
Kevin Bustos Erbuzzi <126516608+KevinBUstosErbuzzi@users.noreply.github.com>
2023-03-28 17:36:41 -0300

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-let miImage = document.querySelector(".Foto img");
-miImage.onclick = function () {let miSrc = miImage.getAttribute("src");
-if (miSrc === "img/5.jpg") {miImage.setAttribute("src","img/2.jpg");}
-else {miImage.setAttribute("src","img/5.jpg");}}
+let miImage = document.querySelector(".Foto img"););
+
+miImage.onclick = function () {
+    
+    let miSrc = miImage.getAttribute("src");
+    
+    if (miSrc === "img/5.jpg")
+        miImage.setAttribute("src","img/2.jpg");
+    else
+        miImage.setAttribute("src","img/5.jpg");
+}
