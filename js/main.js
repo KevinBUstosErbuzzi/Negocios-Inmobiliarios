a755e0f04a08a2ade2556c017abaa9d5a9fedff6
KevinBUstosErbuzzi <you@example.com>
2024-05-27 20:13:21 -0300

    api

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ miImage.onclick = function () {
     else
         miImage.setAttribute("src","img/5.jpg");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+const apiUrl = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+const galeria = document.getElementById('galeria');
+data.forEach(photo => {
+const img = document.createElement('img');
+img.src = photo.url;
+img.alt = photo.title;
+galeria.appendChild(img);
+});
+})
+.catch(error => console.error('Error al cargar las fotos:', error));
+});
